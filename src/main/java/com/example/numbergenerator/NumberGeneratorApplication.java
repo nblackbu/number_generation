a0d15018835e80fc6c5c5d38dda705505f70b9c1
nblackbu <sitdikovaelvinaa96@gmail.com>
2023-02-12 17:53:02 +0300

    12.02

--- src/main/java/com/example/numbergenerator/NumberGeneratorApplication.java
+++ src/main/java/com/example/numbergenerator/NumberGeneratorApplication.java
@@ -1,5 +1,9 @@
 package com.example.numbergenerator;
 
+import com.example.numbergenerator.controller.NumberController;
+import com.example.numbergenerator.service.GeneratorNumberService;
+import com.example.numbergenerator.service.NumberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
