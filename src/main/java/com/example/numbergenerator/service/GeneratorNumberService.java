8e11199902278fa4ecfbf464cdc70f40706e0b0b
nblackbu <sitdikovaelvinaa96@gmail.com>
2023-02-12 17:53:02 +0300

    12.02

--- src/main/java/com/example/numbergenerator/service/GeneratorNumberService.java
+++ src/main/java/com/example/numbergenerator/service/GeneratorNumberService.java
@@ -11,11 +11,10 @@ public class GeneratorNumberService {
 
     private final String[] letters = {"A", "Е", "Т", "О", "Р", "Н", "У", "К", "Х", "С", "В", "М"};
 
-    public List<String> returnAutoMobileNumberInOrder() {
+    public synchronized List<String> listGenerator() {
 
         String str1 = null;
         List<String> list1 = new ArrayList<>();
-        int counter = 0;
 
         for (int y = 0; y < letters.length; y++) {
             for (int z = 0; z < letters.length; z++) {
@@ -34,25 +33,12 @@ public class GeneratorNumberService {
     }
 
     public List<String> returnAutoMobileNumberRandom() {
-
-        String str2 = null;
-        List<String> list2 = new ArrayList<>();
-        int counter2 = 0;
-
-        for (int y = 0; y < letters.length; y++) {
-            for (int z = 0; z < letters.length; z++) {
-                for (int x = 0; x < letters.length; x++) {
-                    for (int i = 0; i >= 0 && i <= 999; i++) {
-                        if (i >= 0 && i <= 9) str2 = "00" + i;
-                        if (i >= 10 && i <= 99) str2 = "0" + i;
-                        if (i >= 100 && i <= 999) str2 = "" + i;
-                        String rez = letters[x] + str2 + letters[z] + letters[y] + " 116 RUS";
-                        list2.add(rez);
-                    }
-                }
-            }
-        }
-        Collections.shuffle(list2);
-        return list2;
+        List<String> listRandom = listGenerator();
+        Collections.shuffle(listRandom);
+        return listRandom;
+    }
+    public List<String> returnAutoMobileNumberInOrder() {
+        List<String> listInOrder = listGenerator();
+        return listInOrder;
     }
 }
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import com.example.numbergenerator.service.GeneratorNumberService;
+import com.example.numbergenerator.service.NumberService;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AppTest {
+    @Test
+    public void contextLoads() {
+
+        GeneratorNumberService carNumberListGeneratorService = new GeneratorNumberService();
+        List<String> s = carNumberListGeneratorService.returnAutoMobileNumberInOrder();
+        List<String> r = carNumberListGeneratorService.returnAutoMobileNumberRandom();
+
+        NumberService numberService = new NumberService(carNumberListGeneratorService.returnAutoMobileNumberRandom(),carNumberListGeneratorService.returnAutoMobileNumberInOrder());
+
+        int size = carNumberListGeneratorService.returnAutoMobileNumberInOrder().size() + 1;
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(numberService.getNextCarNumberRandom());
+        }
+    }
+}
